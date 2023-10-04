ef02af1cc1ecdd3d7a1ee3e27868bc692ae5895f
Aman Malviya <amanmalviya@Amans-MacBook-Air.local>
2023-10-04 23:02:39 +0530

    Initiated a new react frontend in vite

---
+++ Frontend-Vite/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-center'>
+        Hello World
+      </h2>
+    </>
+  )
+}
+
+export default App
